1516a9cb5a094f2b626957eb848d86da49106429
Yuval Horowitz <yuval.horowitz@gmail.com>
2025-11-09 16:06:56 +0200

    setup command for simualation

---
+++ simulation_setup.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyroomacoustics as pra
+from numpy import hamming
+
+print("All libraries imported successfully!")
+print(f"Using pyroomacoustics version: {pra.__version__}")
+
+# This is a good place to start building your room
+# (We'll do this next)
